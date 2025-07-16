c392ac44ea0afc1bc649db1f116e35f185cf0fe7
Marggy-Gitau <gitaumarggy@gmail.com>
2025-07-16 16:28:11 +0300

    MongoDB configuration

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI); // <== USE MONGO_URI here
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
@@ -14,4 +11,5 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
 
